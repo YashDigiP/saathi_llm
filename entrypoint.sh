5b8aadaa6fcb8b6025ce2cf4cb8a03e2ac86174f
YashDigiP <151629163+YashDigiP@users.noreply.github.com>
2025-05-17 13:28:53 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
-#!/bin/bash
-echo "Downloading models from GCS..."
-gsutil -m cp -r gs://llm_models_ollama/ollama-models/* /root/.ollama/models/
+if [ ! -d "/home/ollama/.ollama/models" ]; then
+  echo "⏬ Downloading models from GCS..."
+  gsutil cp -r gs://your-bucket/.ollama /home/ollama/
+  chown -R ollama:ollama /home/ollama/.ollama
+else
+  echo "✅ Models already loaded, skipping download."
+fi
